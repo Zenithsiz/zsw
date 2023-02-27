a89cd9ba6c8782896f974bc3562ebc39e5f25ad0
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-02-27 06:45:08 +0000

    Locker no longer clones arcs on each resource.

--- zsw/src/shared/lockers.rs
+++ zsw/src/shared/lockers.rs
@@ -62,70 +62,81 @@ macro define_locker(
 	)*
 
 ) {
-	$(
-		/// Locker
-		///
-		/// # Thread safety
-		/// Multiple lockers should not be created per task
-		#[derive(Debug)]
-		pub struct $LockerName<const STATE: usize = 0> {
-			$(
-				$lock_name: Arc<Mutex<$lock_ty>>,
-			)*
-		}
-
-		impl<const STATE: usize> $LockerName<STATE> {
-			/// Creates a new locker
-			pub fn $new($( $lock_name: Arc<Mutex<$lock_ty>> ),*) -> Self {
-				Self { $( $lock_name, )* }
+	paste::paste! {
+		$(
+			/// Locker inner
+			#[derive(Debug)]
+			pub struct [< $LockerName Inner >] {
+				$(
+					$lock_name: Arc<Mutex<$lock_ty>>,
+				)*
 			}
 
-			/// Locks a resource
-			#[track_caller]
-			pub async fn $resource<'locker, R>(
-				&'locker mut self,
-			) -> (Resource<'locker, R>, <Self as Locker<R>>::Next<'locker>)
+			/// Locker
+			///
+			/// # Thread safety
+			/// Multiple lockers should not be created per task
+			#[derive(Debug)]
+			pub struct $LockerName<const STATE: usize = 0> {
+				inner: &'static [< $LockerName Inner >],
+			}
+
+			impl<const STATE: usize> $LockerName<STATE> {
+				/// Creates a new locker
+				pub fn $new($( $lock_name: Arc<Mutex<$lock_ty>> ),*) -> Self {
+					// TODO: Don't leak and instead drop it only when dropping when `STATE == 0`.
+					let inner = [< $LockerName Inner >] {
+						$( $lock_name, )*
+					};
+					let inner = Box::leak(Box::new(inner));
+
+					Self { inner }
+				}
+
+				/// Locks a resource
+				#[track_caller]
+				pub async fn $resource<'locker, R>(
+					&'locker mut self,
+				) -> (Resource<'locker, R>, <Self as Locker<R>>::Next<'locker>)
+				where
+					Self: Locker<R>,
+					R: 'locker,
+				{
+					self.lock_resource().await
+				}
+			}
+
+			#[duplicate::duplicate_item(
+				ResourceTy field NEXT_STATE;
+				$(
+					[$lock_ty] [$lock_name] [{ $lock_idx + 1 }];
+				)*
+			)]
+			impl<const CUR_STATE: usize> Locker<ResourceTy> for $LockerName<CUR_STATE>
 			where
-				Self: Locker<R>,
-				R: 'locker,
+				where_assert!(NEXT_STATE > CUR_STATE):,
 			{
-				self.lock_resource().await
+				type Next<'locker> = $LockerName<NEXT_STATE>;
+
+				#[track_caller]
+				async fn lock_resource<'locker>(&'locker mut self) -> (Resource<ResourceTy>, Self::Next<'locker>)
+				where
+					ResourceTy: 'locker,
+				{
+					#[cfg(feature = "locker-trace")]
+					tracing::trace!(resource = ?std::any::type_name::<ResourceTy>(), backtrace = %std::backtrace::Backtrace::force_capture(), "Locking resource");
+					let guard = self.inner.field.lock().await;
+
+					#[cfg(feature = "locker-trace")]
+					tracing::trace!(resource = ?std::any::type_name::<ResourceTy>(), backtrace = %std::backtrace::Backtrace::force_capture(), "Locked resource");
+					let resource = Resource::new(guard);
+
+					let locker = $LockerName {
+						inner: self.inner
+					};
+					(resource, locker)
+				}
 			}
-		}
-
-		#[duplicate::duplicate_item(
-			ResourceTy field NEXT_STATE;
-			$(
-				[$lock_ty] [$lock_name] [{ $lock_idx + 1 }];
-			)*
-		)]
-		impl<const CUR_STATE: usize> Locker<ResourceTy> for $LockerName<CUR_STATE>
-		where
-			where_assert!(NEXT_STATE > CUR_STATE):,
-		{
-			type Next<'locker> = $LockerName<NEXT_STATE>;
-
-			#[track_caller]
-			async fn lock_resource<'locker>(&'locker mut self) -> (Resource<ResourceTy>, Self::Next<'locker>)
-			where
-				ResourceTy: 'locker,
-			{
-				#[cfg(feature = "locker-trace")]
-				tracing::trace!(resource = ?std::any::type_name::<ResourceTy>(), backtrace = %std::backtrace::Backtrace::force_capture(), "Locking resource");
-				let guard = self.field.lock().await;
-
-				#[cfg(feature = "locker-trace")]
-				tracing::trace!(resource = ?std::any::type_name::<ResourceTy>(), backtrace = %std::backtrace::Backtrace::force_capture(), "Locked resource");
-				let resource = Resource::new(guard);
-
-				// TODO: Not clone an `Arc` here each time.
-				let locker = $LockerName {
-					$(
-						$lock_name: Arc::clone(&self.$lock_name),
-					)*
-				};
-				(resource, locker)
-			}
-		}
-	)*
+		)*
+	}
 }
