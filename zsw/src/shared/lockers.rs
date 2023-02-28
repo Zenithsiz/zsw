d0d0000d922d0c17170c81f2e954fdddc5c211d8
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-02-28 17:11:06 +0000

    Deduplicated function identifier in lockers macro.

--- zsw/src/shared/lockers.rs
+++ zsw/src/shared/lockers.rs
@@ -23,80 +23,79 @@ type PanelsUpdaterMeetupRenderer = ();
 type EguiPainterMeetupRenderer = (Vec<egui::ClippedPrimitive>, egui::TexturesDelta);
 
 define_locker! {
-	LoadDefaultPanelGroupLocker {
-		fn new(...) -> Self;
+	fn new(...) -> Self;
 
+	fn lock(...) -> ...;
+
+	fn read(...) -> ...;
+	fn upgradable_read(...) -> ...;
+	fn write(...) -> ...;
+
+	fn send(...) -> ...;
+
+	LoadDefaultPanelGroupLocker {
 		async_mutex {
-			fn lock(...) -> ...;
 			CurPanelGroup = [ 0 ] => 1,
 			PanelsRendererShader = [ 0 ] => 1,
 		}
 
 		async_rwlock {
-			fn read(...) -> ...;
-			fn upgradable_read(...) -> ...;
-			fn write(...) -> ...;
-
 			PlaylistsManager = [ 0 ] => 1,
 		}
 	}
 
 	RendererLocker {
-		fn new(...) -> Self;
-
 		async_mutex {
-			fn lock(...) -> ...;
 			CurPanelGroup = [ 0 ] => 1,
 			PanelsRendererShader = [ 0 1 ] => 2,
 		}
 	}
 
 	PanelsUpdaterLocker {
-		fn new(...) -> Self;
-
 		async_mutex {
-			fn lock(...) -> ...;
 			CurPanelGroup = [ 0 ] => 1,
 		}
 
 		meetup_sender {
-			fn send(...) -> ...;
 			PanelsUpdaterMeetupRenderer = [ 0 ]
 		}
 	}
 
 	EguiPainterLocker {
-		fn new(...) -> Self;
-
 		async_mutex {
-			fn lock(...) -> ...;
 			CurPanelGroup = [ 0 ] => 1,
 			PanelsRendererShader = [ 0 1 ] => 2,
 		}
 
 		async_rwlock {
-			fn read(...) -> ...;
-			fn upgradable_read(...) -> ...;
-			fn write(...) -> ...;
-
 			PlaylistsManager = [ 0 1 2 ] => 3,
 		}
 
 		meetup_sender {
-			fn send(...) -> ...;
 			EguiPainterMeetupRenderer = [ 0 ],
 		}
 	}
 }
 
 macro define_locker(
+	fn $new:ident(...) -> Self;
+
+	// Mutex
+	fn $lock_async_mutex:ident(...) -> ...;
+
+	// RwLock
+	fn $lock_async_rwlock_read:ident(...) -> ...;
+	fn $lock_async_rwlock_upgradable_read:ident(...) -> ...;
+	fn $lock_async_rwlock_write:ident(...) -> ...;
+
+	// meetup::Sender
+	fn $send_meetup_sender:ident(...) -> ...;
+
 	$(
 		$LockerName:ident {
-			fn $new:ident(...) -> Self;
 
 			$(
 				async_mutex {
-					fn $lock_async_mutex:ident(...) -> ...;
 					$(
 						$async_mutex_ty:ty = [ $( $async_mutex_prev:literal )* ] => $async_mutex_next:literal
 					),*
@@ -106,9 +105,6 @@ macro define_locker(
 
 			$(
 				async_rwlock {
-					fn $lock_async_rwlock_read:ident(...) -> ...;
-					fn $lock_async_rwlock_upgradable_read:ident(...) -> ...;
-					fn $lock_async_rwlock_write:ident(...) -> ...;
 					$(
 						$async_rwlock_ty:ty = [ $( $async_rwlock_prev:literal )* ] => $async_rwlock_next:literal
 					),*
@@ -118,7 +114,6 @@ macro define_locker(
 
 			$(
 				meetup_sender {
-					fn $send_meetup_sender:ident(...) -> ...;
 					$(
 						$meetup_sender_ty:ty = [ $( $meetup_sender_prev:literal )* ]
 					),*
@@ -146,85 +141,79 @@ macro define_locker(
 				//       In this case, we'd likely associate it with a task-id so we can make sure that the
 				//       previous task never re-locks again or similar?
 				pub fn $new() -> Self {
-					static IS_CREATED: AtomicBool = AtomicBool::new(false);
+					static CREATED: AtomicBool = AtomicBool::new(false);
 					assert!(
-						!IS_CREATED.swap(true, atomic::Ordering::AcqRel),
+						!CREATED.swap(true, atomic::Ordering::AcqRel),
 						"Cannot create locker twice"
 					);
 
 					Self(())
 				}
 
-				$(
-					/// Locks the async mutex `R`
-					#[track_caller]
-					pub async fn $lock_async_mutex<'locker, 'mutex, R>(
-						&'locker mut self,
-						mutex: &'mutex Mutex<R>,
-					) -> (MutexGuard<'mutex, R>, <Self as AsyncMutexLocker<R>>::Next<'locker>)
-					where
-						Self: AsyncMutexLocker<R>,
-						R: 'locker,
-					{
-						self.lock_resource(mutex).await
-					}
-				)?
+				/// Locks the async mutex `R`
+				#[track_caller]
+				pub async fn $lock_async_mutex<'locker, 'mutex, R>(
+					&'locker mut self,
+					mutex: &'mutex Mutex<R>,
+				) -> (MutexGuard<'mutex, R>, <Self as AsyncMutexLocker<R>>::Next<'locker>)
+				where
+					Self: AsyncMutexLocker<R>,
+					R: 'locker,
+				{
+					self.lock_resource(mutex).await
+				}
 
-				$(
-					/// Locks the async rwlock `R` for reading
-					#[track_caller]
-					pub async fn $lock_async_rwlock_read<'locker, 'rwlock, R>(
-						&'locker mut self,
-						rwlock: &'rwlock RwLock<R>,
-					) -> (RwLockReadGuard<'rwlock, R>, <Self as AsyncRwLockLocker<R>>::Next<'locker>)
-					where
-						Self: AsyncRwLockLocker<R>,
-						R: 'locker,
-					{
-						self.lock_read_resource(rwlock).await
-					}
+				/// Locks the async rwlock `R` for reading
+				#[track_caller]
+				pub async fn $lock_async_rwlock_read<'locker, 'rwlock, R>(
+					&'locker mut self,
+					rwlock: &'rwlock RwLock<R>,
+				) -> (RwLockReadGuard<'rwlock, R>, <Self as AsyncRwLockLocker<R>>::Next<'locker>)
+				where
+					Self: AsyncRwLockLocker<R>,
+					R: 'locker,
+				{
+					self.lock_read_resource(rwlock).await
+				}
 
-					/// Locks the async rwlock `R` for an upgradable reading
-					#[track_caller]
-					pub async fn $lock_async_rwlock_upgradable_read<'locker, 'rwlock, R>(
-						&'locker mut self,
-						rwlock: &'rwlock RwLock<R>,
-					) -> (RwLockUpgradableReadGuard<'rwlock, R>, <Self as AsyncRwLockLocker<R>>::Next<'locker>)
-					where
-						Self: AsyncRwLockLocker<R>,
-						R: 'locker,
-					{
-						self.lock_upgradable_read_resource(rwlock).await
-					}
+				/// Locks the async rwlock `R` for an upgradable reading
+				#[track_caller]
+				pub async fn $lock_async_rwlock_upgradable_read<'locker, 'rwlock, R>(
+					&'locker mut self,
+					rwlock: &'rwlock RwLock<R>,
+				) -> (RwLockUpgradableReadGuard<'rwlock, R>, <Self as AsyncRwLockLocker<R>>::Next<'locker>)
+				where
+					Self: AsyncRwLockLocker<R>,
+					R: 'locker,
+				{
+					self.lock_upgradable_read_resource(rwlock).await
+				}
 
-					/// Locks the async rwlock `R` for writing
-					#[track_caller]
-					pub async fn $lock_async_rwlock_write<'locker, 'rwlock, R>(
-						&'locker mut self,
-						rwlock: &'rwlock RwLock<R>,
-					) -> (RwLockWriteGuard<'rwlock, R>, <Self as AsyncRwLockLocker<R>>::Next<'locker>)
-					where
-						Self: AsyncRwLockLocker<R>,
-						R: 'locker,
-					{
-						self.lock_write_resource(rwlock).await
-					}
-				)?
+				/// Locks the async rwlock `R` for writing
+				#[track_caller]
+				pub async fn $lock_async_rwlock_write<'locker, 'rwlock, R>(
+					&'locker mut self,
+					rwlock: &'rwlock RwLock<R>,
+				) -> (RwLockWriteGuard<'rwlock, R>, <Self as AsyncRwLockLocker<R>>::Next<'locker>)
+				where
+					Self: AsyncRwLockLocker<R>,
+					R: 'locker,
+				{
+					self.lock_write_resource(rwlock).await
+				}
 
-				$(
-					/// Sends the resource `R` to it's meetup channel
-					#[track_caller]
-					pub async fn $send_meetup_sender<R>(
-						&mut self,
-						tx: &meetup::Sender<R>,
-						resource: R,
-					)
-					where
-						Self: MeetupSenderLocker<R>
-					{
-						self.send_resource(tx, resource).await;
-					}
-				)?
+				/// Sends the resource `R` to it's meetup channel
+				#[track_caller]
+				pub async fn $send_meetup_sender<R>(
+					&mut self,
+					tx: &meetup::Sender<R>,
+					resource: R,
+				)
+				where
+					Self: MeetupSenderLocker<R>
+				{
+					self.send_resource(tx, resource).await;
+				}
 			}
 
 			// Async mutexes
