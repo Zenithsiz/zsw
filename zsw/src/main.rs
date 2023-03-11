Zenithsiz/zsw

commit 5ef9e81d9f195f9f4619aa97d9de3a8ab4f50bd7

Updated to `rustc 1.70.0-nightly (ff4b772f8 2023-03-10)`.

zsw/src/main.rs
@@ -196,52 +196,46 @@ async fn run(dirs: &ProjectDirs, config: &Config) -> Result<(), AppError> {
 
 	self::spawn_task("Load playlists", {
 		let shared = Arc::clone(&shared);
-		async move { self::load_playlists(AsyncLocker::new(), shared).await }
+		self::load_playlists(AsyncLocker::new(), shared)
 	});
 
 	self::spawn_task("Load default panel group", {
 		let shared = Arc::clone(&shared);
 		let default_panel_group = config.default_panel_group.clone();
-		async move { self::load_default_panel_group(default_panel_group, AsyncLocker::new(), shared).await }
+		self::load_default_panel_group(default_panel_group, AsyncLocker::new(), shared)
 	});
 
 	self::spawn_task("Renderer", {
 		let shared = Arc::clone(&shared);
-		async move {
-			self::renderer(
-				shared,
-				AsyncLocker::new(),
-				wgpu_renderer,
-				panels_renderer,
-				egui_renderer,
-				egui_painter_output_rx,
-				panels_updater_output_rx,
-			)
-			.await
-		}
+		self::renderer(
+			shared,
+			AsyncLocker::new(),
+			wgpu_renderer,
+			panels_renderer,
+			egui_renderer,
+			egui_painter_output_rx,
+			panels_updater_output_rx,
+		)
 	});
 
 	self::spawn_task("Panels updater", {
 		let shared = Arc::clone(&shared);
 		let panels_updater_output_tx = PanelsUpdaterMeetupSender::new(panels_updater_output_tx);
-		async move { self::panels_updater(shared, AsyncLocker::new(), panels_updater_output_tx).await }
+		self::panels_updater(shared, AsyncLocker::new(), panels_updater_output_tx)
 	});
 
-	self::spawn_task("Image loader", async move { image_loader.run().await });
+	self::spawn_task("Image loader", image_loader.run());
 
 	self::spawn_task("Egui painter", {
 		let shared = Arc::clone(&shared);
 		let egui_painter_output_tx = EguiPainterRendererMeetupSender::new(egui_painter_output_tx);
-		async move {
-			self::egui_painter(
-				shared,
-				AsyncLocker::new(),
-				egui_painter,
-				settings_menu,
-				egui_painter_output_tx,
-			)
-			.await
-		}
+		self::egui_painter(
+			shared,
+			AsyncLocker::new(),
+			egui_painter,
+			settings_menu,
+			egui_painter_output_tx,
+		)
 	});
 
 	// Then run the event loop on this thread

zsw/src/logger.rs
@@ -19,6 +19,7 @@ use {
 /// Initializes the logger
 ///
 /// Logs to both stderr and `log_file`, if any
+#[expect(clippy::cognitive_complexity)] // TODO: Refactor?
 pub fn init(log_file: Option<&Path>) {
 	// Create the terminal layer
 	let term_use_colors = self::colors_enabled();

zsw/src/wgpu_wrapper.rs
@@ -23,7 +23,7 @@ pub struct WgpuShared {
 pub async fn create(window: Arc<Window>) -> Result<(WgpuShared, WgpuRenderer), AppError> {
 	// Create the surface and adapter
 	// SAFETY: `WgpuRenderer` keeps an `Arc<Window>` to ensure it stays alive for as long as the surface
-	let (surface, adapter) = unsafe { self::create_surface_and_adapter(&window).await? };
+	let (surface, adapter) = unsafe { self::create_surface_and_adapter(&window) }.await?;
 
 	// Then create the device and it's queue
 	let (device, queue) = self::create_device(&adapter).await?;

commit 86a4fbbb6c90196bf4221d07174bc07c70dda73b

Fixed locker validation failing after 5ef9e81d9f195f9f4619aa97d9de3a8ab4f50bd7.

zsw/src/main.rs
@@ -196,32 +196,45 @@ async fn run(dirs: &ProjectDirs, config: &Config) -> Result<(), AppError> {
 
 	self::spawn_task("Load playlists", {
 		let shared = Arc::clone(&shared);
-		self::load_playlists(AsyncLocker::new(), shared)
+		async move {
+			let locker = AsyncLocker::new();
+			self::load_playlists(locker, shared).await
+		}
 	});
 
 	self::spawn_task("Load default panel group", {
 		let shared = Arc::clone(&shared);
 		let default_panel_group = config.default_panel_group.clone();
-		self::load_default_panel_group(default_panel_group, AsyncLocker::new(), shared)
+		async move {
+			let locker = AsyncLocker::new();
+			self::load_default_panel_group(default_panel_group, locker, shared).await
+		}
 	});
 
 	self::spawn_task("Renderer", {
 		let shared = Arc::clone(&shared);
-		self::renderer(
-			shared,
-			AsyncLocker::new(),
-			wgpu_renderer,
-			panels_renderer,
-			egui_renderer,
-			egui_painter_output_rx,
-			panels_updater_output_rx,
-		)
+		async move {
+			let locker = AsyncLocker::new();
+			self::renderer(
+				shared,
+				locker,
+				wgpu_renderer,
+				panels_renderer,
+				egui_renderer,
+				egui_painter_output_rx,
+				panels_updater_output_rx,
+			)
+			.await
+		}
 	});
 
 	self::spawn_task("Panels updater", {
 		let shared = Arc::clone(&shared);
 		let panels_updater_output_tx = PanelsUpdaterMeetupSender::new(panels_updater_output_tx);
-		self::panels_updater(shared, AsyncLocker::new(), panels_updater_output_tx)
+		async move {
+			let locker = AsyncLocker::new();
+			self::panels_updater(shared, locker, panels_updater_output_tx).await
+		}
 	});
 
 	self::spawn_task("Image loader", image_loader.run());
@@ -229,13 +242,10 @@ async fn run(dirs: &ProjectDirs, config: &Config) -> Result<(), AppError> {
 	self::spawn_task("Egui painter", {
 		let shared = Arc::clone(&shared);
 		let egui_painter_output_tx = EguiPainterRendererMeetupSender::new(egui_painter_output_tx);
-		self::egui_painter(
-			shared,
-			AsyncLocker::new(),
-			egui_painter,
-			settings_menu,
-			egui_painter_output_tx,
-		)
+		async move {
+			let locker = AsyncLocker::new();
+			self::egui_painter(shared, locker, egui_painter, settings_menu, egui_painter_output_tx).await
+		}
 	});
 
 	// Then run the event loop on this thread
@@ -243,7 +253,7 @@ async fn run(dirs: &ProjectDirs, config: &Config) -> Result<(), AppError> {
 		event_loop.run_return(|event, _, control_flow| {
 			*control_flow = winit::event_loop::ControlFlow::Wait;
 
-			#[expect(clippy::single_match)] // We'll add more in the future
+			#[allow(clippy::single_match)] // We'll add more in the future
 			match event {
 				winit::event::Event::WindowEvent { ref event, .. } => match *event {
 					winit::event::WindowEvent::Resized(size) => shared.last_resize.store(Some(Resize { size })),