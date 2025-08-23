72c7eaa2640963c1730e793ee062d3b6fc8ca36b
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2025-08-23 01:50:33 +0100

    Playlists are now cached.

--- zsw/src/playlist/loader.rs
+++ zsw/src/playlist/loader.rs
@@ -3,7 +3,9 @@
 // Imports
 use {
 	super::{Playlist, PlaylistItem, PlaylistItemKind, PlaylistName, ser},
-	std::path::PathBuf,
+	futures::lock::Mutex,
+	std::{collections::HashMap, path::PathBuf, sync::Arc},
+	tokio::sync::OnceCell,
 	zsw_util::PathAppendExt,
 	zutil_app_error::{AppError, Context},
 };
@@ -13,43 +15,60 @@ use {
 pub struct PlaylistsLoader {
 	/// Playlists directory
 	root: PathBuf,
+
+	/// Loaded playlists
+	// TODO: Limit the size of this?
+	playlists: Mutex<HashMap<PlaylistName, Arc<OnceCell<Arc<Playlist>>>>>,
 }
 
 impl PlaylistsLoader {
 	/// Creates a new playlists loader
 	pub fn new(root: PathBuf) -> Self {
-		Self { root }
+		Self {
+			root,
+			playlists: Mutex::new(HashMap::new()),
+		}
 	}
 
 	/// Loads a playlist by name
-	pub async fn load(&self, playlist_name: PlaylistName) -> Result<Playlist, AppError> {
-		// Try to read the file
-		let playlist_path = self.playlist_path(&playlist_name);
-		tracing::debug!(%playlist_name, ?playlist_path, "Loading playlist");
-		let playlist_toml = tokio::fs::read_to_string(playlist_path)
+	pub async fn load(&self, playlist_name: PlaylistName) -> Result<Arc<Playlist>, AppError> {
+		self.playlists
+			.lock()
 			.await
-			.context("Unable to open file")?;
+			.entry(playlist_name.clone())
+			.or_insert_with(|| Arc::new(OnceCell::new()))
+			.get_or_try_init(async move || {
+				// Try to read the file
+				let playlist_path = self.playlist_path(&playlist_name);
+				tracing::debug!(%playlist_name, ?playlist_path, "Loading playlist");
+				let playlist_toml = tokio::fs::read_to_string(playlist_path)
+					.await
+					.context("Unable to open file")?;
 
-		// And parse it
-		let playlist = toml::from_str::<ser::Playlist>(&playlist_toml).context("Unable to parse playlist")?;
-		let playlist = Playlist {
-			items: playlist
-				.items
-				.into_iter()
-				.map(|item| PlaylistItem {
-					enabled: item.enabled,
-					kind:    match item.kind {
-						ser::PlaylistItemKind::Directory { path, recursive } => PlaylistItemKind::Directory {
-							path: path.into(),
-							recursive,
-						},
-						ser::PlaylistItemKind::File { path } => PlaylistItemKind::File { path: path.into() },
-					},
-				})
-				.collect(),
-		};
+				// And parse it
+				let playlist = toml::from_str::<ser::Playlist>(&playlist_toml).context("Unable to parse playlist")?;
+				let playlist = Playlist {
+					items: playlist
+						.items
+						.into_iter()
+						.map(|item| PlaylistItem {
+							enabled: item.enabled,
+							kind:    match item.kind {
+								ser::PlaylistItemKind::Directory { path, recursive } => PlaylistItemKind::Directory {
+									path: path.into(),
+									recursive,
+								},
+								ser::PlaylistItemKind::File { path } => PlaylistItemKind::File { path: path.into() },
+							},
+						})
+						.collect(),
+				};
+				tracing::info!("Loaded playlist {playlist_name:?}");
 
-		Ok(playlist)
+				Ok(Arc::new(playlist))
+			})
+			.await
+			.map(Arc::clone)
 	}
 
 	/// Returns a playlist's path
