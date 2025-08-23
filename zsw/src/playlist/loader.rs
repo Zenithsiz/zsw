0883416ec3ffe2c5fae722926c68bd1cf5d95364
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2025-08-23 01:38:41 +0100

    Moved playlist loader into it's own module.

---
+++ zsw/src/playlist/loader.rs
@@ -0,0 +1,59 @@
+//! Playlist player
+
+// Imports
+use {
+	super::{Playlist, PlaylistItem, PlaylistItemKind, PlaylistName, ser},
+	std::path::PathBuf,
+	zsw_util::PathAppendExt,
+	zutil_app_error::{AppError, Context},
+};
+
+/// Playlist loader
+#[derive(Debug)]
+pub struct PlaylistsLoader {
+	/// Playlists directory
+	root: PathBuf,
+}
+
+impl PlaylistsLoader {
+	/// Creates a new playlists loader
+	pub fn new(root: PathBuf) -> Self {
+		Self { root }
+	}
+
+	/// Loads a playlist by name
+	pub async fn load(&self, playlist_name: PlaylistName) -> Result<Playlist, AppError> {
+		// Try to read the file
+		let playlist_path = self.playlist_path(&playlist_name);
+		tracing::debug!(%playlist_name, ?playlist_path, "Loading playlist");
+		let playlist_toml = tokio::fs::read_to_string(playlist_path)
+			.await
+			.context("Unable to open file")?;
+
+		// And parse it
+		let playlist = toml::from_str::<ser::Playlist>(&playlist_toml).context("Unable to parse playlist")?;
+		let playlist = Playlist {
+			items: playlist
+				.items
+				.into_iter()
+				.map(|item| PlaylistItem {
+					enabled: item.enabled,
+					kind:    match item.kind {
+						ser::PlaylistItemKind::Directory { path, recursive } => PlaylistItemKind::Directory {
+							path: path.into(),
+							recursive,
+						},
+						ser::PlaylistItemKind::File { path } => PlaylistItemKind::File { path: path.into() },
+					},
+				})
+				.collect(),
+		};
+
+		Ok(playlist)
+	}
+
+	/// Returns a playlist's path
+	pub fn playlist_path(&self, name: &PlaylistName) -> PathBuf {
+		self.root.join(&*name.0).with_appended(".toml")
+	}
+}
