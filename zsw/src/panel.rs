65e8fefb1f90a91c942cb4dd41f61c1cefa45e3d
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2025-08-23 01:39:57 +0100

    Moved panel loader into it's own module.

--- zsw/src/panel.rs
+++ zsw/src/panel.rs
@@ -3,6 +3,7 @@
 // Modules
 mod geometry;
 mod images;
+mod loader;
 mod renderer;
 mod ser;
 mod state;
@@ -11,66 +12,19 @@ mod state;
 pub use self::{
 	geometry::PanelGeometry,
 	images::{PanelImage, PanelImages},
+	loader::PanelsLoader,
 	renderer::{PanelShader, PanelsGeometryUniforms, PanelsRenderer, PanelsRendererLayouts},
 	state::PanelState,
 };
 
 // Imports
 use {
-	crate::{AppError, image_loader::ImageRequester, playlist::PlaylistPlayer},
+	crate::{image_loader::ImageRequester, playlist::PlaylistPlayer},
 	core::{borrow::Borrow, fmt},
-	std::{path::PathBuf, sync::Arc},
-	zsw_util::{PathAppendExt, Rect},
+	std::sync::Arc,
+	zsw_util::Rect,
 	zsw_wgpu::WgpuShared,
-	zutil_app_error::Context,
 };
-
-/// Panels loader
-#[derive(Debug)]
-pub struct PanelsLoader {
-	/// Panels directory
-	root: PathBuf,
-}
-
-impl PanelsLoader {
-	/// Creates a new panels loader
-	pub fn new(root: PathBuf) -> Self {
-		Self { root }
-	}
-
-	/// Loads a panel from a name.
-	///
-	/// If the panel isn't for this window, returns `Ok(None)`
-	pub async fn load(&self, panel_name: PanelName, shader: PanelShader) -> Result<Panel, AppError> {
-		// Try to read the file
-		let panel_path = self.panel_path(&panel_name);
-		tracing::debug!(%panel_name, ?panel_path, "Loading panel");
-		let panel_toml = tokio::fs::read_to_string(panel_path)
-			.await
-			.context("Unable to open file")?;
-
-		// Then parse it
-		let panel = toml::from_str::<ser::Panel>(&panel_toml).context("Unable to parse panel")?;
-
-		// Finally convert it
-		let geometries = panel.geometries.into_iter().map(|geometry| geometry.geometry).collect();
-		let state = PanelState {
-			paused:     false,
-			progress:   0,
-			duration:   panel.state.duration,
-			fade_point: panel.state.fade_point,
-		};
-
-		let panel = Panel::new(panel_name.clone(), geometries, state, shader);
-
-		Ok(panel)
-	}
-
-	/// Returns a panel's path
-	pub fn panel_path(&self, name: &PanelName) -> PathBuf {
-		self.root.join(&*name.0).with_appended(".toml")
-	}
-}
 
 /// Panel
 #[derive(Debug)]
